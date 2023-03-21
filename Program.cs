2add82e0ffaac4159df0ff957f3717c7c7cb9b3c
User <User@DESKTOP-805DRN8>
2023-03-21 17:51:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringUpr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(input[0]);
+            int decimal2 = int.Parse(input[1]);
+
+            string result = "";
+            if (decimal2 > 0)
+            {
+                int remainder = decimal2 % n;
+                result = remainder.ToString() + result;
+                decimal2 /= n;
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
